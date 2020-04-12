82e99b93937ba5c51af18b4eda343c2dca3eb5fa
JamesSc0tt <jamesscott.js95@gmail.com>
2020-04-12 13:52:33 +0100

    :fsn_main: new settings file

---
+++ fsn_main/server_settings/sh_settings.lua
@@ -0,0 +1,11 @@
+GM = {} -- Leave this here
+GM['server'] = {}
+GM['currency'] = {}
+
+GM['version'] = '2.1.0'; -- Do not change this value
+
+GM['server'].name = ':FSN: Framework Dev Server'; -- Server/community name
+GM['server'].url = 'https://fsn.rocks'; -- URL of server (also ban screen)
+
+GM['currency'].icon = '$';
+GM['currency'].name = 'USD';
