4c384e4364ea5ed52bd229e4a9aed930b27719d4
JamesSc0tt <jamesscott.js95@gmail.com>
2020-04-12 14:08:18 +0100

    :fsn_main: add rules to sh_settings.lua

--- fsn_main/server_settings/sh_settings.lua
+++ fsn_main/server_settings/sh_settings.lua
@@ -4,6 +4,16 @@ GM['currency'] = {}
 
 GM['version'] = '2.1.0'; -- Do not change this value
 
+GM['rules'] = { -- The GUI is desgined to support 7 rules. Adding more is not recommended.
+	[1] = 'Join Our Discord!',
+	[2] = 'Cheating, exploiting or hacking of any kind is not permitted, this includes external crosshairs.',
+	[3] = 'Do not RDM/VDM - You have to have initiated in roleplay with the individual first.',
+	[4] = 'Twitch TOS - Do not intentionally violate Twitch terms of service while playing.',
+	[5] = 'Combat Logging - Do not disconnect from the server/respawn to avoid a roleplay/admin scenario.',
+	[6] = 'EMS - Do not lie to an EMS about your injuries or respawn whilst in EMS custody.',
+	[7] = 'Power Gaming - Do not use mechanics that do not exist in the server to gain an advantage.',
+}
+
 GM['server'].name = ':FSN: Framework Dev Server'; -- Server/community name
 GM['server'].url = 'https://fsn.rocks'; -- URL of server (also ban screen)
 
