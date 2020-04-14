4f0aec211b5da80e5538799b9b75fec170b8bd35
Joshua <github@itzcrutchie.com>
2020-04-14 10:50:48 -0500

    fix __resource.lua (#11)
    Fix __resource.lua on fsn_bank and fsn_cargarage after the update for the settings file in main.

--- fsn_bank/__resource.lua
+++ fsn_bank/__resource.lua
@@ -7,6 +7,9 @@ server_script '@fsn_main/server_settings/sh_settings.lua'
 server_script '@mysql-async/lib/MySQL.lua'
 --[[/	:FSN:	\]]--
 
+client_script 'client.lua'
+server_script 'server.lua'
+
 ui_page 'gui/index.html'
 files({
   'gui/index.html',
