94e98ee23634d9f8c732b158dda7b559022dfe37
JamesSc0tt <jamesscott.js95@gmail.com>
2021-06-20 21:11:48 +0100

    main - util object

--- fsn3_main/character/cl_character.lua
+++ fsn3_main/character/cl_character.lua
@@ -1,7 +1,10 @@
-
+local UTIL = nil
+TriggerEvent('fsn:object:util', function(o)
+	UTIL = o
+end)
 
 RegisterNetEvent('fsn3:softLog')
 AddEventHandler('fsn3:softLog', function()
 	-- handle characters/restarting/swapping
-	
+
 end)
