c768b042de956769340d5c1cf73c72d193de679f
JamesSc0tt <jamesscott.js95@gmail.com>
2021-06-20 15:48:44 +0100

    main - manifest

---
+++ fsn3_main/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'James Scott'
+description ''
+version '0.0.0'
+
+client_scripts {
+	'world/cl_world.lua',
+	'player/cl_player.lua',
+	'character/cl_character.lua'
+}
+
+server_scripts {
+	'character/sv_character.lua'
+}
