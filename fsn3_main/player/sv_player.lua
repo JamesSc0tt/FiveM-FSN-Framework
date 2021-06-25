3021765badf7f3664ce4faea70e3a19ab0976259
JamesSc0tt <jamesscott.js95@gmail.com>
2021-06-25 16:59:14 +0100

    player table stuff

--- fsn3_main/player/sv_player.lua
+++ fsn3_main/player/sv_player.lua
@@ -5,5 +5,14 @@ local players = {}
 PLY.functions = {}
 
 PLY.functions.getplayerid = function(src)
-	-- work out player id	
+	local p = false
+	if players[src] then
+		p = players[src]
+	end
+	for k,v in pairs(players) do
+		if v ~= p then
+			-- remove existing old players incase of reconnect
+			players[k] = nil
+		end
+	end
 end
